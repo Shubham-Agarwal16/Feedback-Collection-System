3c94ae4b89a08226f447b4bbcae97ce533809243
Shubham Agarwal <16shubhamagarwal@gmail.com>
2022-11-12 01:32:10 +0530

    first commit

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+  { label: "Survey Title", name: "Title" },
+  { label: "Survey Line", name: "subject" },
+  { label: "Email Body", name: "body" },
+  { label: "Recipient List", name: "recipients" },
+];
